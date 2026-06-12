4bec48770c8e4a77033371ee9b17f97e945ddd2d
KonshinHaoshin <KonshinHaoshin@gmail.com>
2026-06-12 14:54:50 +0800

    add npm

--- src/data/characterAliases.ts
+++ src/data/characterAliases.ts
@@ -1,10 +1,12 @@
+import { CUSTOM_CHARACTER_ALIASES } from './customCharacters';
+
 export const CHARACTER_ALIASES: Record<string, string[]> = {
   // MyGO!!!!!
-  '332': ['高松灯', '高松燈', '灯', '燈', 'tomori', 'takamatsu tomori', 'tomorin'],
-  '333': ['千早爱音', '千早愛音', '爱音', '愛音', 'anon', 'chihaya anon'],
-  '334': ['要乐奈', '要樂奈', '要楽奈', '乐奈', '樂奈', '楽奈', 'rana', 'raana', 'kaname rana', 'kaname raana'],
-  '335': ['长崎爽世', '長崎爽世', '長崎そよ', '爽世', 'soyo', 'nagasaki soyo'],
-  '336': ['椎名立希', '立希', 'taki', 'shiina taki'],
+  '36': ['高松灯', '高松燈', '灯', '燈', 'tomori', 'takamatsu tomori', 'tomorin'],
+  '37': ['千早爱音', '千早愛音', '爱音', '愛音', 'anon', 'anon chihaya', 'chihaya anon'],
+  '38': ['要乐奈', '要樂奈', '要楽奈', '乐奈', '樂奈', '楽奈', 'rana', 'raana', 'kaname rana', 'kaname raana'],
+  '39': ['长崎爽世', '長崎爽世', '長崎そよ', '爽世', 'soyo', 'nagasaki soyo'],
+  '40': ['椎名立希', '立希', 'taki', 'shina taki', 'shiina taki'],
 
   // Ave Mujica
   '337': ['三角初华', '三角初華', '初华', '初華', 'uika', 'misumi uika'],
@@ -12,4 +14,6 @@ export const CHARACTER_ALIASES: Record<string, string[]> = {
   '339': ['八幡海铃', '八幡海鈴', '海铃', '海鈴', 'umiri', 'yahata umiri'],
   '340': ['祐天寺若麦', '祐天寺若麥', '祐天寺喵梦', '喵梦', '喵夢', '若麦', '若麥', 'nyamu', 'yutenji nyamu'],
   '341': ['丰川祥子', '豐川祥子', '豊川祥子', '祥子', 'sakiko', 'togawa sakiko'],
+
+  ...CUSTOM_CHARACTER_ALIASES,
 };
